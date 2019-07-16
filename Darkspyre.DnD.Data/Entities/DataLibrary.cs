7311908a40c41e59d5eb4a9b81f04e59b35fa3d9
Brandon Mathis <brandonm@atiba.com>
2019-07-16 08:25:36 -0500

    Cleanup

--- Darkspyre.DnD.Data/Entities/DataLibrary.cs
+++ Darkspyre.DnD.Data/Entities/DataLibrary.cs
@@ -8,10 +8,15 @@ namespace Darkspyre.DnD.Data.Entities
     public class DataLibrary
     {
         //public List<EquipmentItem> Items { get; set; } = new List<EquipmentItem>();
-      //  public List<Class> Classes { get; set; } = new List<Class>();
-     //   public List<Race> Races { get; set; } = new List<Race>();
-        public List<Spell> Spells { get; set; } = new List<Spell>();
+        //  public List<Class> Classes { get; set; } = new List<Class>();
+        //   public List<Race> Races { get; set; } = new List<Race>();
+
+        public List<Background> Backgrounds { get; set; } = new List<Background>();
+        public List<CharClass> CharacterClasses { get; set; } = new List<CharClass>();
+        public List<CharSheet> CharacterSheets { get; set; } = new List<CharSheet>();
         public List<NonPlayerCharacter> NonPlayerCharacters { get; set; } = new List<NonPlayerCharacter>();
+        public List<Spell> Spells { get; set; } = new List<Spell>();
+        
 
     }
 }
--- Darkspyre.DnD.SpellTool.Win/MainForm2.cs
+++ Darkspyre.DnD.SpellTool.Win/MainForm2.cs
@@ -21,5 +21,62 @@ namespace Darkspyre.DnD.SpellTool.Win
             InitializeComponent();
             
         }
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private Controls.SpellsManager spellsManager1;
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm2));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.spellsManager1 = new Darkspyre.DnD.SpellTool.Win.Controls.SpellsManager(dataLibrary);
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1164, 556);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.spellsManager1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1156, 530);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Spells";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // spellsManager1
+            // 
+            this.spellsManager1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.spellsManager1.Location = new System.Drawing.Point(3, 3);
+            this.spellsManager1.Name = "spellsManager1";
+            this.spellsManager1.Size = new System.Drawing.Size(1150, 524);
+            this.spellsManager1.TabIndex = 0;
+            // 
+            // MainForm2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1188, 580);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm2";
+            this.Text = "FG Data Manager v2.0";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
     }
 }
